cc6c1666d739f2ee7ce105e7f4f70b4617ae5a9e
julia-mareike <jnmareike@gmail.com>
2020-03-29 15:15:09 +1300

    Add mongoDB connection

---
+++ server/src/config/databaseConnection.js
@@ -0,0 +1,22 @@
+const MongoClient = require('mongodb').MongoClient
+
+let mongoDB
+
+const setupDB = callback => {
+    const uri = `mongodb+srv://dbAdmin:${process.env.MONGO_ADMIN_PASSWORD}@cluster0-ysmyo.gcp.mongodb.net/test?retryWrites=true&w=majority`
+
+    MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+        mongoDB = client.db('jambu-server')
+        if (err) {
+            return callback(err)
+        } else {
+            return callback('Database is sweet')
+        }
+    })
+}
+
+const getDB = () => {
+    return mongoDB
+}
+
+module.exports = { setupDB, getDB }
--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,14 @@
+require('dotenv').config()
+
+console.log(process.env)
 const express = require('express')
 const cors = require('cors')
 const app = express()
 const graphqlHTTP = require('express-graphql')
+const { setupDB } = require('./config/databaseConnection')
+
+// Call in database connection
+setupDB(value => console.log(value))
 
 const schema = {
 
