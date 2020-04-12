9446fa58cfbe28e04b7d425f8eb955f737eeae92
julia-mareike <jnmareike@gmail.com>
2020-04-13 10:09:22 +1200

    Sort notes by dateCreated or dateModified

--- client/src/components/DisplayNotes.jsx
+++ client/src/components/DisplayNotes.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Button } from 'semantic-ui-react'
 
 import Note from './Note.jsx'
 
 const DisplayNotes = ({ notes }) => {
+
+  const [sortFilter, setSortFilter] = useState('dateCreated')
+  const sortedNotes = notes.slice().sort((a, b) => a[sortFilter] - b[sortFilter])
+
+  const buttonFilter = sortFilter === 'dateCreated' ? 'dateModified' : 'dateCreated'
+
   return (
-    notes.map(note => {
-      return <Note key={note._id} note={note} />
-    })
+    <React.Fragment>
+      <Button onClick={() => setSortFilter(buttonFilter)}>Sort!</Button>
+      {sortedNotes.map(note => {
+        return <Note key={note._id} note={note} />
+      })}
+    </React.Fragment>
   )
 }
 
